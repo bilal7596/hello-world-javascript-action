7b0e9ce6c8f68f91a235812b70990205033e4331
Bilal Basha M <bilalbasha.developer@gmail.com>
2024-12-14 17:34:32 +0530

    Javascript call Workflow updated

--- dist/index.js
+++ dist/index.js
@@ -31815,16 +31815,20 @@ const core = __nccwpck_require__(8478);
 const github = __nccwpck_require__(7782);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    console.log(process.env.secrets.TEST_SECRET);
+    console.log(process.env.secrets.GITHUB_TOKEN);
+    console.log(process.env.secrets.TOKEN);
+    console.log(process.env.GITHUB_WORKSPACE);
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
 
 module.exports = __webpack_exports__;
